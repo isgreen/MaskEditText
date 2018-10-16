51151f64129ebb4ff45678276b18c8c598b96059
Everdes Soares <isgreen@madeinweb.com.br>
2018-10-16 20:30:33 -0300

    included method for set text with mask

--- app/src/main/java/br/com/isgreen/maskedittext/MaskEditText.kt
+++ app/src/main/java/br/com/isgreen/maskedittext/MaskEditText.kt
@@ -83,6 +83,15 @@ class MaskEditText : AppCompatEditText {
                 .replace(Regex("\\s+"), "")
     }
 
+    fun setText(text: String) {
+        for (i in 0 until mCurrentMask.length) {
+            if (text.length > i) {
+                val newText = super.getText().toString()
+                super.setText(String.format("%s%s", newText, text.subSequence(i, i + 1)))
+            }
+        }
+    }
+
     private val onTextChange = object : TextWatcher {
 
         private var isUpdating: Boolean = false
